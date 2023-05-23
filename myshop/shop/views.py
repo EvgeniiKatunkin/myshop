c4c21509e80316c96f063836387650af04cea66d
Evgenii <evgeniikatunkin@gmail.com>
2023-05-23 11:50:35 +0200

    Create context_processors.py for cart context processor to show information about products in cart on every page. Finish cart functionality.

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -6,7 +6,8 @@ from .models import Category, Product
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, 'shop/product/detail.html', {'product': product})
+    cart_product_form = CartAddProductForm()
+    return render(request, 'shop/product/detail.html', {'product': product, 'cart_product_form': cart_product_form})
 
 
 def product_list(request, category_slug=None):
@@ -16,7 +17,5 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    cart_product_form = CartAddProductForm()
     return render(request, 'shop/product/list.html',
-                  {'category': category, 'categories': categories, 'products': products,
-                   'cart_product_form': cart_product_form})
+                  {'category': category, 'categories': categories, 'products': products})
