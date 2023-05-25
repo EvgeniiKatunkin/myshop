68e1a5c9d2f85bf4a7e0ea5827cbc00d4f06aa95
Evgenii <evgeniikatunkin@gmail.com>
2023-05-25 16:22:41 +0200

    Created recommendation for products feature.

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 
-from cart.forms import CartAddProductForm
 from .models import Category, Product
+from .recommender import Recommender
+from cart.forms import CartAddProductForm
 
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     cart_product_form = CartAddProductForm()
-    return render(request, 'shop/product/detail.html', {'product': product, 'cart_product_form': cart_product_form})
+    r = Recommender()
+    recommended_products = r.suggest_products_for([product], 4)
+    return render(request, 'shop/product/detail.html', {'product': product, 'cart_product_form': cart_product_form,
+                                                        'recommended_products': recommended_products})
 
 
 def product_list(request, category_slug=None):
